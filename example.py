10eb913798322fcd026a61b1bfe328dd25ed4b0f
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-01-21 12:30:14 +0100

    reworked example.py

--- example.py
+++ example.py
@@ -10,12 +10,10 @@ time.sleep(0.15)
 timeCtrl.addTime("loaded file")
 time.sleep(0.24)
 timeCtrl.addTime("processed file")
-time.sleep(0.47)
+time.sleep(0.10)
 timeCtrl.addTime("saved file")
 
 # print results
-print("default print: ")
-print(timeCtrl)
-print("-------------------")
-print("custom settings: ")
-print(timeCtrl.print(unit='ms', decimals=3, show_deltas=False, show_notes=False))
+print(timeCtrl)  # default print
+timeCtrl.print(unit='ms', decimals=3, show_deltas=False, show_notes=False)  # custom settings
+timeCtrl.barChart()  # bar chart
